489da082c577679c5e59f7c39f622ebf42fab1eb
neel002b <neelbhalodiya555@gmail.com>
2024-01-22 12:02:15 +0530

    first commit

---
+++ scpirt.js
@@ -0,0 +1,43 @@
+// Variable
+var a = 2, b = 3;
+
+// ternary Operator
+(a == 2 && b == 3) ? console.log("true") : console.log("false");
+(a == 1 || b == 2) ? console.log("true") : console.log("false");
+!(a == 1 || b == 2) ? console.log("true") : console.log("false");
+
+
+// Array
+var car = ["bmw", "volvo"];
+
+// Array in Add
+car.push = "mebenz";
+
+// Array in remove
+car.pop = "volvo";
+
+// object 
+var person = { fname: "neil", lname: "patel", dob: "10-01-02", nation: "indian" };
+
+//Operators
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//Incement ++ and --
+
+
+console.log(a++);
+console.log(++a);
+console.log(b--);
+console.log(--b);
+
+
+//Function 
+
+function myFunction(a, b) {
+    return a * b;
+  }
