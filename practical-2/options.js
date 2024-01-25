c1b94b0bbf65ef0ae34b3ed55f44137bacadef04
neel002b <neelbhalodiya555@gmail.com>
2024-01-25 09:44:19 +0530

    Practical-2

--- practical-2/options.js
+++ practical-2/options.js
@@ -1,12 +1,35 @@
-function table() {
 
+var totalTextBoxes = 0;
 
-    var value = document.getElementById('select').value;
-
-    if (value === 'Add') {
-        var textbox = document.createElement("input");
-        document.body.appendChild(textbox);
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    if (x !== "") {
+        document.getElementById("inputField").style.display = "block";
+    } else {
+        document.getElementById("inputField").style.display = "none";
     }
+}
 
+function addOrRemoveTextBoxes() {
+    var num = document.getElementById("numField").value;
+    var container = document.getElementById("textBoxes");
+    var x = document.getElementById("mySelect").value;
 
+    if (x === "1") {
+        for (var i = 0; i < num; i++) {
+            var input = document.createElement("input");
+            input.type = "text";
+            container.appendChild(input);
+            container.appendChild(document.createElement("br"));
+            totalTextBoxes++;
+        }
+    } else if (x === "2") {
+        for (var i = 0; i < num; i++) {
+            if (container.hasChildNodes()) {
+                container.removeChild(container.lastChild); // remove the line break
+                container.removeChild(container.lastChild); // remove the input field
+                totalTextBoxes--;
+            }
+        }
+    }
 }
